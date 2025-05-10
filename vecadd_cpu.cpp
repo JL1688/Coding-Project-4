59b6e05599cd84ef00f08441e1bc640233487b3c
JL1688 <jliu42@sfsu.edu>
2025-05-09 17:31:56 -0700

    update vecadd

--- vecadd_cpu.cpp
+++ vecadd_cpu.cpp
@@ -7,59 +7,55 @@
 // 1-sentence description here: implementation of vector addition code (CPU version)
 //
 
+#include <iostream>
+#include <math.h>
+#include <chrono>
 
- /*
-  * Input arguments:
-  * - n: The dimension of the square matrix A and the rows of X and Y.
-  * - A: n-by-n matrix stored in row-major format.
-  * - x: n by 1 vector
-  * - y: n by 1 vector (output)
-  */
+// function to add the elements of two arrays
+// Input arguments:
+// - n: The number of elements in arrays x and y
+// - x: The first array of n elements
+// - y: The second array of n elements (result vector).
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+    y[i] = x[i] + y[i];
+}
 
-  #include <iostream>
-  #include <math.h>
-  #include <chrono>
-   
-  // function to add the elements of two arrays
-  void add(int n, float *x, float *y)
+int main(void)
+{
+  int N = 1 << 29; // 512M elements
+
+  float *x = new float[N];
+  float *y = new float[N];
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++)
   {
-   for (int i = 0; i < n; i++)
-       y[i] = x[i] + y[i];
+    x[i] = 1.0f;
+    y[i] = 2.0f;
   }
-   
-  int main(void)
-  {
-   int N = 1<<29; // 512M elements
-   
-   float *x = new float[N];
-   float *y = new float[N];
-   
-   // initialize x and y arrays on the host
-   for (int i = 0; i < N; i++) {
-     x[i] = 1.0f;
-     y[i] = 2.0f;
-   }
-   
-   // start timer code 
-   std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
 
-   // Run kernel on 1M elements on the CPU
-   add(N, x, y);
+  // start timer code
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
 
-   // end timer code, and print out the elapsed time for this problem size
-   std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
-   std::chrono::duration<double> elapsed = end_time - start_time;
-   std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
-   
-   // Check for errors (all values should be 3.0f)
-   float maxError = 0.0f;
-   for (int i = 0; i < N; i++)
-     maxError = fmax(maxError, fabs(y[i]-3.0f));
-   std::cout << "Max error: " << maxError << std::endl;
-   
-   // Free memory
-   delete [] x;
-   delete [] y;
-   
-   return 0;
-  }
+  // Run kernel on 1M elements on the CPU
+  add(N, x, y);
+
+  // end timer code, and print out the elapsed time for this problem size
+  std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = end_time - start_time;
+  std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i] - 3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  delete[] x;
+  delete[] y;
+
+  return 0;
+}
